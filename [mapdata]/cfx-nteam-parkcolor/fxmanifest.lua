c9d3151c589e12a20b9849ca5160b55082f20234
Cushgaming <135035600+Cushgaming@users.noreply.github.com>
2025-07-30 20:48:47 -0600

    color park added

---
+++ [mapdata]/cfx-nteam-parkcolor/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+version '8.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+
+
+data_file('DLC_ITYP_REQUEST')('stream/ytyp/parkcolorytyp.ytyp')
+
+
+
+dependency '/assetpacks'
