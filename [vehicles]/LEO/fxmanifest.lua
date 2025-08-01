2fbb977c0e34f12714c032a3df9449cc5e7bc5d3
Austincardev <austincardev@gmail.com>
2025-07-31 21:55:14 -0400

    Update fxmanifest.lua

--- [vehicles]/LEO/fxmanifest.lua
+++ [vehicles]/LEO/fxmanifest.lua
@@ -5,16 +5,16 @@ author 'Snow-Customs'
 description 'Enus Windsor Lux'
 version '2.0.0'
 
-data_file 'HANDLING_FILE' 'data/handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
-data_file 'CARCOLS_FILE' 'data/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
 
 
 files {
-  'data/handling.meta',
-  'data/vehicles.meta',
-  'data/carcols.meta',
-  'data/carvariations.meta',
+  'data/**/handling.meta',
+  'data/**/vehicles.meta',
+  'data/**/carcols.meta',
+  'data/**/carvariations.meta',
 }
 
