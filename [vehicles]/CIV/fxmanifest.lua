798a82fa21c6249d1453a7833f427fcffbe93307
Austincardev <austincardev@gmail.com>
2025-08-01 21:27:26 -0400

    updated vehicle file

---
+++ [vehicles]/CIV/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'Snow-Customs'
+description 'Enus Windsor Lux'
+version '2.0.0'
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+
+
+files {
+  'data/**/handling.meta',
+  'data/**/vehicles.meta',
+  'data/**/carcols.meta',
+  'data/**/carvariations.meta',
+}
+
